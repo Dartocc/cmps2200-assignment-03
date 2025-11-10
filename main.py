acee1945886748a66f8afae3b7755dbe8d01b456
Dartocc <dtocci@tulane.edu>
2025-11-10 17:52:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -80,11 +80,17 @@ def fast_align_MED(S, T):
                 dp[i][j] = dp[i - 1][j - 1]
                 backtrack[i][j] = 'match'
             else:
-                if dp[i - 1][j] <= dp[i][j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j]
+                delete = dp[i - 1][j] + 1
+                insert = dp[i][j - 1] + 1
+                substitute = dp[i - 1][j - 1] + 1
+
+                dp[i][j] = min(delete, insert, substitute)
+
+                if dp[i][j] == substitute:
+                    backtrack[i][j] = 'sub'
+                elif dp[i][j] == delete:
                     backtrack[i][j] = 'del'
                 else:
-                    dp[i][j] = 1 + dp[i][j - 1]
                     backtrack[i][j] = 'ins'
 
     # Reconstruct alignment
@@ -93,22 +99,19 @@ def fast_align_MED(S, T):
     i, j = m, n
 
     while i > 0 or j > 0:
-        move = backtrack[i][j]
-        if move == 'match':
+        move = backtrack[i][j] if i > 0 and j > 0 else None
+        if move in ('match', 'sub'):
             aligned_S = S[i - 1] + aligned_S
             aligned_T = T[j - 1] + aligned_T
             i -= 1
             j -= 1
-        elif move == 'del':
+        elif move == 'del' or (j == 0 and i > 0):
             aligned_S = S[i - 1] + aligned_S
             aligned_T = "-" + aligned_T
             i -= 1
-        elif move == 'ins':
+        elif move == 'ins' or (i == 0 and j > 0):
             aligned_S = "-" + aligned_S
             aligned_T = T[j - 1] + aligned_T
             j -= 1
 
-    return (dp[m][n], aligned_S, aligned_T)
-    
-    
-
+    return aligned_S, aligned_T
